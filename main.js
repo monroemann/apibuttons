5062a5d31632bbd1b553a741e0122c2aedfa7e26
Monroe Mann <findingsummer@aol.com>
2021-05-30 18:07:25 -0400

    Turned it into a chuck norris generator

--- main.js
+++ main.js
@@ -2,22 +2,22 @@ const textButton = document.getElementById('number');
 const apiButton = document.getElementById('chuck');
 const outputDiv = document.getElementById('output');
 
-const textURL = 'https://numbersapi.com/random';
+// const textURL = 'http://numbersapi.com/random';
 const apiURL = 'https://api.chucknorris.io/jokes/random';
 
-textButton.addEventListener('click', () => {
-    fetch(textURL)
-    .then( response => {
-        outputDiv.innerHTML = 'Waiting for response...';
-    if(response.ok) {
-        return response;
-    }
-        throw Error(response.statusText);
-    })
-    .then( response => response.text() )
-    .then( text => outputDiv.innerText = text )
-    .catch( error => console.log('There was an error:', error))
-},false);
+// textButton.addEventListener('click', () => {
+//     fetch(textURL)
+//     .then( response => {
+//         outputDiv.innerHTML = 'Waiting for response...';
+//     if(response.ok) {
+//         return response;
+//     }
+//         throw Error(response.statusText);
+//     })
+//     .then( response => response.text() )
+//     .then( text => outputDiv.innerText = text )
+//     .catch( error => console.log('There was an error:', error))
+// },false);
 
 apiButton.addEventListener('click', () => {
     fetch(apiURL)
